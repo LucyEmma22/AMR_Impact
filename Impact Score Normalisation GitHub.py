974ec2d2b74d88a06cf88b18130d5bfac6645b27
Lucy Binsted <78858900+LucyEmma22@users.noreply.github.com>
2023-11-26 21:12:14 +0000

    Update Impact Score Normalisation GitHub.py

--- Impact Score Normalisation GitHub.py
+++ Impact Score Normalisation GitHub.py
@@ -1,167 +1,222 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.ticker as ticker
 import statsmodels.api as sm
 from scipy.stats.stats import spearmanr
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-from sklearn import metrics
+from scipy.stats import gaussian_kde
+from sklearn.metrics import r2_score
+import ast
+import os
+os.chdir("/Users/s1995754/Library/CloudStorage/OneDrive-UniversityofEdinburgh/PhD Year 3/AMR Impact") # set working directory
 
 ##############################################################################################################################
+bins = 50
 
-def normalise_by_age(data, response, min_year, max_year):
-    data = data[data['year'] <= max_year] # Remove papers newer than max_year
-    data = data[data['year'] >= min_year] # Remove papers older than min_year
-
-    # Plot Histogram of Age
-    plt.hist(data['age'].tolist(), bins=20)
-    plt.xlabel('Age (Years)')
-    plt.ylabel("Frequency")
-    plt.show()
-
-    # Plot Histogram of Response
-    plt.hist(data[response].tolist(), bins=20)
-    plt.xlabel(response)
-    plt.ylabel("Frequency")
-    plt.show()
-
+def normalise_by_age(data, response, ylab):
     # Spline model
-    knots = np.linspace(data['age'].min(), data['age'].max(), num=8)[1:-1] # Define list of knots for the spline model
-    spline_model = sm.GLM.from_formula(f'{response} ~ bs(age, knots = knots)', data=data).fit() # Spline model using age of paper to predict Altmetric Score
+    df = data[['PMID','age', response]].dropna()
+    knots = np.linspace(df['age'].min(), df['age'].max(), num=8)[1:-1] # Define list of knots for the spline model
+    spline_model = sm.GLM.from_formula(f'{response} ~ bs(age, knots = knots)', data=df).fit() # Spline model using age of paper to predict Altmetric Score
     preds = spline_model.get_prediction().summary_frame(alpha=0.05)['mean'] # Spline model output
-    data = data.merge(preds, left_index=True, right_index=True) # Join spline model results with data
-    data['residual'] = data[response] - data['mean'] # Calculate each papers residual from the spline model 
-    line_data = data[['age','mean']].drop_duplicates().sort_values(by='age') # Select columns and drop duplicates for plotting the line
+    df = df.merge(preds, left_index=True, right_index=True) # Join spline model results with data
+    df[f'{response}_residual'] = df[response] - df['mean'] # Calculate each papers residual from the spline model 
+    line_data = df[['age','mean']].drop_duplicates().sort_values(by='age') # Select columns and drop duplicates for plotting the line
+    data = data.merge(df[['PMID',f'{response}_residual']], on='PMID', how='outer') # Add normalised score to original data
 
-    plt.hist2d(data['age'], data[response], bins=50, cmap='viridis',norm=colors.LogNorm())
+    # Plot
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fontsize = 16
+    
+    plt.hist2d(df['age'], df[response], bins=bins, cmap='viridis',norm=colors.LogNorm())
+    plt.plot(line_data['age'], line_data['mean'], c='red', linewidth = '3')
+    plt.xlabel('Age (Years)', fontsize=fontsize)
+    plt.ylabel(ylab, fontsize=fontsize)
     plt.colorbar(label='Density')
-    plt.plot(line_data['age'], line_data['mean'], linewidth=1, c="black")
-    plt.xlabel('Age (Years)')
-    plt.ylabel(response)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    fig.tight_layout()
+    plt.savefig(f'Figures/{response}_vs_age.pdf', format='pdf')
     plt.show()
-
-    # Linear model
-    model = np.polyfit(data['age'], data[response], 1)
-    model_predict = np.polyval(model, data['age'])
-    plt.hist2d(data['age'], data[response], bins=50, cmap='viridis',norm=colors.LogNorm())
-    plt.colorbar(label='Density')
-    plt.plot(data['age'], model_predict, c='black')
-    plt.xlabel('Age (Years)')
-    plt.ylabel(response)
-    plt.show()
-    return data
-
-
-# Define papers as high or low impact depending on various thresholds 
-def define_impact(data):
-    data = data.sort_values(by ='residual', ascending=False) # Sort by residual
-    data['order'] = list(range(0, len(data))) # add a column called orde
-    impact_levels = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5] # define proportion of papers to be high impact
-    for threshold in impact_levels:
-        data[f'impact_{threshold}'] = np.where(data['order'] < (round(len(data)*threshold)), 1, 0)
-    data.drop(['order'], axis=1, inplace=True)
-    data.sort_index(inplace=True)
     return data
 
 
 # Plot Cumultive Distribution and Calculate Gini Coefficient
-def plot_cumulative_distribution(data, response):
-    data = data.sort_values(by ='residual', ascending=False) # Sort by residual
-    data['cumulative'] = data[response].cumsum()
-    impact_levels = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5] # define proportion of papers to be high impact
-    plt.plot(np.array(data['cumulative']))
-    x_values = [round(len(data['cumulative'])*i) for i in impact_levels]
-    y_values = [data['cumulative'].iloc[x] for x in x_values]
-    for i in range(len(x_values)):
-        x = x_values[i]
-        y = y_values[i]
-        plt.plot([x, x], [0, y], color='red', linestyle='--')
-        plt.plot([0, x], [y, y], color='red', linestyle='--')
-    plt.xlabel("Number of Abstracts")
-    plt.ylabel(response)
-    plt.show()
-    for impact,y in zip(impact_levels,y_values):
-        print (str(round(impact*100)) + "% high impact = ", str(np.round(y/max(data['cumulative'])*100,1)) + "% of citations")
-        
-    array = np.array(data[response]).flatten()
-    if np.amin(array) < 0:
-        array -= np.amin(array) # Values cannot be negative:
+def plot_cumulative_distribution(data, response, ylab):
+    df = data[['PMID','age', response]].dropna()
+    df = df.sort_values(by=response, ascending=False) # Sort by residual
+    df['cumulative'] = df[response].cumsum()
+    
+    # Plot
+    fig, ax = plt.subplots(figsize=(5, 5))
+    fontsize = 16
+
+    plt.plot(np.array(df['cumulative']))
+    plt.xlabel('Number of Abstracts', fontsize=fontsize)
+    plt.ylabel(ylab, fontsize=fontsize)
+    plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+    plt.gca().ticklabel_format(axis='y', style='sci', scilimits=(5,5))
+    plt.gca().xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+    plt.gca().ticklabel_format(axis='x', style='sci', scilimits=(4,4))
+    
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    fig.tight_layout()
+    plt.savefig(f'Figures/cumulative_{response}.pdf', format='pdf')
+    plt.show() 
+    
+    # Calculate Gini Index
+    array = np.array(df[response]).flatten()
     array = array + 0.0000001 # Values cannot be 0
     array = np.sort(array) # Values must be sorted
     index = np.arange(1,array.shape[0]+1) # Index per array element
     n = array.shape[0] # Number of array elements
-    print ('Gini Coefficient: ' + str(round((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)),5))) # Gini coefficient
-    
-    data.drop(['cumulative'], axis=1, inplace=True)
-    data.sort_index(inplace=True)
+    gini = (np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)) 
+    return gini
 
-##############################################################################################################################
 
-# Import Text
-preprocessed_text = pd.read_csv("preprocessed_text_filtered.csv").dropna(subset='abstract') 
-preprocessed_text['age'] = (2023 + 9/12) - preprocessed_text['year'] + preprocessed_text['month']/12 # Calculate age (from Sept 2023)
+def plot_relationship (data, x, y, xlab, ylab):
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fontsize = 16
 
-# Normalise Citations
-citation_data = pd.read_csv('citation_data.csv').merge(preprocessed_text, on='PMID', how='inner')
-citation_data['log_cpy'] = np.log((citation_data['number_citations'] / citation_data['age'])+1) # Calculate citations per year
-citation_data = normalise_by_age(citation_data, 'log_cpy', 1970, 2021)
-citation_data = define_impact(citation_data)
-plot_cumulative_distribution(citation_data, 'number_citations')
-citation_data.to_csv("preprocessed_normalised_citations.csv", index=False)
-
-# Normalise Altmetrics
-altmetric_data = pd.read_csv('altmetric_data.csv')
-mention_columns = [col for col in altmetric_data.columns if col.endswith('mentions')]
-altmetric_data = altmetric_data[['PubMed ID', 'Altmetric Attention Score', 'Number of Mendeley readers', 'Number of Dimensions citations'] + mention_columns].rename(columns={'PubMed ID': 'PMID', 'Altmetric Attention Score':'altmetric_score'}).merge(preprocessed_text, on='PMID', how='inner')
-for x in ['Policy mentions', 'Patent mentions']:
-    altmetric_data.loc[altmetric_data[x] != 0, x] = 1
-altmetric_data = altmetric_data.rename(columns={'Policy mentions':'policy_mentions', 'Patent mentions':'patent_mentions'})
-altmetric_data['log_altmetric_score'] = np.log(altmetric_data['altmetric_score']+1) # Rescale altmetric score
-altmetric_data = normalise_by_age(altmetric_data, 'log_altmetric_score', 1970, 2024)
-altmetric_data = define_impact(altmetric_data)
-plot_cumulative_distribution(altmetric_data, 'altmetric_score')
-altmetric_data.to_csv("preprocessed_normalised_altmetrics.csv", index=False)
-
-##############################################################################################################################
-
-# Showing that citation rate and altmetric score are highly correlated
-
-def plot_relationship (data, x, y):
-    model = np.polyfit(data[x], data[y], 1)
-    model_predict = np.polyval(model, data[x])
-    plt.hist2d(data[x], data[y], bins=100, cmap='viridis',norm=colors.LogNorm())
+    df = data[[x, y]].dropna()
+    model = np.polyfit(df[x], df[y], 1)
+    model_predict = np.polyval(model, df[x])
+    r_squared = r2_score(df[y], model_predict)
+    plt.hist2d(df[x], df[y], bins=bins, cmap='viridis',norm=colors.LogNorm())
+    plt.plot(df[x], model_predict, c='red', linewidth = '3')
+    plt.xlabel(xlab,fontsize=fontsize)
+    plt.ylabel(ylab, fontsize=fontsize)
     plt.colorbar(label='Density')
-    plt.plot(data[x], model_predict, c='black')
-    plt.xlabel(x)
-    plt.ylabel(y)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    fig.tight_layout()
+    plt.savefig(f'Figures/{x}_{y}_correlation.pdf', format='pdf')
     plt.show()
-    print('Linear Model Slope: ' , str(np.round(model[0],3)))
-    print('Spearman Correlation: ', str(np.round(spearmanr(data[x], data[y]),3)))
     
-def plot_confusion_matrix(data, x, y):
-    cm = confusion_matrix(data[y], data[x], labels=[0, 1])
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=[0, 1], yticklabels=[0, 1])
-    plt.xlabel(x)
-    plt.ylabel(y)
+    print('Linear Model Slope: ' , str(model[0]))
+    print('Spearman Correlation: ', str(spearmanr(df[x], df[y])))
+    print('R-squared: ', str((r_squared)))
+    
+    
+def plot_distribution(variable, name):
+    variable = variable.dropna()
+    
+    fig, ax = plt.subplots(figsize=(5, 5))
+    fontsize = 16
+    
+    kde = gaussian_kde(variable, bw_method=0.2)
+    x = np.linspace(min(variable), max(variable), 100)
+    y = kde(x)
+    plt.plot(x, y, color='blue', label='KDE')
+    plt.fill_between(x, 0, y, alpha=0.3, color='blue')
+    plt.xlabel(name, fontsize=fontsize)
+    plt.ylabel('Density', fontsize=fontsize)
+    
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    fig.tight_layout()
+    plt.savefig(f'Figures/{name}_distribution.pdf', format='pdf')
     plt.show()
-    print('Accuracy = %.3f' % metrics.accuracy_score(data[y], data[x]))
-    print('Classification report:')
-    print(metrics.classification_report(data[y], data[x]))
+ 
+    
+def plot_frequency (variable, name):
+    df = pd.DataFrame(data[variable].explode().value_counts()).reset_index()
+    
+    fig, ax = plt.subplots(figsize=(5, 5))
+    fontsize = 14
+    
+    plt.bar(df['index'], np.log(df[variable]), color ='blue', width = 0.4)
+    plt.xlabel(name, fontsize=fontsize)
+    plt.ylabel('Log (Frequency)',fontsize=fontsize)
+    plt.xticks(rotation = 90)
+    
+    plt.xticks(fontsize=4)
+    plt.yticks(fontsize=fontsize)
+    fig.tight_layout()
+    plt.savefig(f'Figures/{name}_frequency.pdf', format='pdf')
+    plt.show()   
+ 
+##############################################################################################################################
 
-data = citation_data[['PMID', 'residual']].rename({'residual': 'citation_rate'}, axis=1).merge(altmetric_data[['PMID', 'residual', 'policy_mentions', 'patent_mentions']].rename({'residual': 'altmetric_score'}, axis=1),on='PMID', how='inner')
-plot_relationship(data, 'citation_rate', 'altmetric_score')
-plot_confusion_matrix(data, 'policy_mentions','patent_mentions')
+preprocessed_abstracts = pd.read_csv("2_Data_Processing/preprocessed_abstracts2.csv")
+citation_data = pd.read_csv('1_Data_Retrieval/citation_data2.csv')
+altmetric_data = pd.read_csv('1_Data_Retrieval/altmetric_data2.csv')[['PubMed ID', 'Altmetric Attention Score', 'Policy mentions', 'Patent mentions']].rename(columns={'PubMed ID': 'PMID', 'Altmetric Attention Score':'altmetric_score', 'Policy mentions':'policy_mentions', 'Patent mentions':'patent_mentions'}).drop_duplicates(subset='PMID')
+data = preprocessed_abstracts.merge(citation_data, on='PMID', how='left').merge(altmetric_data, on='PMID', how='left')
+data['article_type'] = data['article_type'] .apply(lambda x: ast.literal_eval(x))
+data = data[data['year'] <= 2022] # Remove papers newer than ...
+data = data[data['year'] >= 1970] # Remove papers older than ...
+data['age'] = (2023 + 9/12) - data['year'] + data['month']/12 # Calculate age (from Sept 2023) 
 
-for x in ['policy_mentions', 'patent_mentions']:
-    data.loc[data[x] != 0, x] = 'Yes'
-    data.loc[data[x] == 0, x] = 'No'
-sns.boxplot(data=data, x="citation_rate", y="policy_mentions",
-            showcaps=False,
-            flierprops={"marker": "none"})
-sns.boxplot(data=data, x="citation_rate", y="patent_mentions",
-            showcaps=False,
-            flierprops={"marker": "none"})
+# Counts
+data['log_cpy'] = np.log(data['number_citations']+1) # Rescale citations
+data['log_alt'] = np.log(data['altmetric_score']+1) # Rescale altmetric score
+data['log_policy_mentions'] = np.log(data['policy_mentions']+1) # Rescale 
+data['log_patent_mentions'] = np.log(data['patent_mentions']+1) # Rescale
+
+##############################################################################################################################
+# Cumulative Distributions and Gini Coefficient
+
+plot_cumulative_distribution(data, 'number_citations', 'Cumulative Citations') # Plot cumulative distribution of citations and calculate gini coefficient
+plot_cumulative_distribution(data, 'altmetric_score', 'Cumulative Altmetric Score') # Plot cumulative distribution of altmetric score and calculate gini coefficient 
+plot_cumulative_distribution(data, 'policy_mentions', 'Cumulative Policy Mentions') # Plot cumulative distribution of altmetric score and calculate gini coefficient 
+plot_cumulative_distribution(data, 'patent_mentions', 'Cumulative Patent Mentions') # Plot cumulative distribution of altmetric score and calculate gini coefficient 
+
+# Means
+print(np.mean(data['number_citations']))
+print(np.mean(data['altmetric_score']))
+print(np.mean(data['patent_mentions']))
+print(np.mean(data['policy_mentions']))
+(data['number_citations'] == 0).sum()
+(data['altmetric_score'] == 0).sum()
+(data['patent_mentions'] == 0).sum()
+(data['policy_mentions'] == 0).sum()
+data['number_citations'].count()
+data['altmetric_score'].count()
+data['patent_mentions'].count()
+data['policy_mentions'].count()
+(data['number_citations'] == 0).sum()/data['number_citations'].count()
+(data['altmetric_score'] == 0).sum()/data['altmetric_score'].count()
+(data['patent_mentions'] == 0).sum()/data['patent_mentions'].count()
+(data['policy_mentions'] == 0).sum()/data['policy_mentions'].count()
+
+
+print(np.mean(data['word_count']))
+print(np.mean(data['title_word_count']))
+print(np.mean(data['number_authors']))
+
+# Distributions
+plot_distribution(data['log_cpy'],'Log (Citations + 1)')
+plot_distribution(data['log_alt'], 'Log (Altmetric Score + 1)')
+plot_distribution(data['log_patent_mentions'], 'Log (Patent Mentions + 1)')
+plot_distribution(data['log_policy_mentions'], 'Log (Policy Mentions + 1)')
+plot_distribution(data['word_count'], 'Abstract Word Count')
+plot_distribution(data['title_word_count'], 'Title Word Count')
+plot_distribution(np.log(data['number_authors']), 'Log (Number Authors)')
+plot_frequency('article_type', 'Article Type')
+plot_frequency('country', 'Country')
+
+
+# Correlations
+plot_relationship(data, 'log_cpy', 'log_alt', 'Log(Citations + 1)', 'Log(Altmetric Score + 1)')
+plot_relationship(data, 'log_cpy', 'log_patent_mentions', 'Log(Citations + 1)', 'Log(Patent Mentions + 1)')
+plot_relationship(data, 'log_cpy', 'log_policy_mentions', 'Log(Citations + 1)', 'Log(Policy Mentions + 1)')
+plot_relationship(data, 'log_alt', 'log_patent_mentions', 'Log(Altmetric Score + 1)', 'Log(Patent Mentions + 1)')
+plot_relationship(data, 'log_alt', 'log_policy_mentions', 'Log(Altmetric Score + 1)', 'Log(Policy Mentions + 1)')
+plot_relationship(data, 'log_patent_mentions', 'log_policy_mentions', 'Log(Patent Mentions + 1)', 'Log(Policy Mentions + 1)')
+
+##############################################################################################################################
+# Normalise by age 
+
+data = normalise_by_age(data, 'log_cpy', 'Log (Citations + 1)') # Normalise citations
+data = normalise_by_age(data, 'log_alt', 'Log (Altmetric Score + 1)') # Normalise altmetric score
+data = normalise_by_age(data, 'log_policy_mentions', 'Log (Policy Mentions + 1)') # Normalise altmetric score
+data = normalise_by_age(data, 'log_patent_mentions', 'Log (Patent Mentions + 1)') # Normalise altmetric score
+
+# Save as CSV
+data.to_csv("2_Data_Processing/preprocessed_normalised_impact.csv", index=False)
