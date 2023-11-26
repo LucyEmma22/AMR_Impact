378d743fc7949c51deeeec95a0fc582a6fa95d17
Lucy Binsted <78858900+LucyEmma22@users.noreply.github.com>
2023-11-26 21:25:17 +0000

    Update AMR Citations Per Year.py

--- AMR Citations Per Year.py
+++ AMR Citations Per Year.py
@@ -10,17 +10,6 @@ os.chdir("/Users/s1995754/Library/CloudStorage/OneDrive-UniversityofEdinburgh/Ph
 plt.style.use('seaborn-v0_8-whitegrid')
 set_matplotlib_formats('svg')
 
-
-data = pd.read_csv("3_Data_Analysis/Prop_AMR_by_Year.csv")
-data = data[data['year']<=2019]
-data = data[data['year']>=1900]
-
-plt.plot(data['year'], data['amr']/data['journal_article'], linewidth=2, c="black")
-plt.xlabel('Year')
-plt.ylabel('Proportion')
-plt.title('Proportion Of PubMed Journal Articles Relating To AMR')
-plt.show()
-
 data = pd.read_csv("3_Data_Analysis/Number_AMR_by_Year.csv")
 data = data[data['Year']<=2020]
 data = data[data['Year']>=1900]
